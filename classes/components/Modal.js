8f681a60e13fc39c808e7952cab04724730d7a63
jurol <jurol@example.com>
2025-07-27 09:25:20 +0200

    Add form validation

--- classes/components/Modal.js
+++ classes/components/Modal.js
@@ -18,6 +18,28 @@ export class Modal {
             this.dialogElem.close();
         });
         this.#form = document.getElementById('add-form')
+        const titleInput = document.querySelector("#title")
+        const authorInput = document.querySelector("#author")
+        const pageInput = document.querySelector("#pages")
+
+        titleInput.addEventListener('input', (e) => {
+            titleInput.setCustomValidity("")
+            if (!titleInput.validity.valid) return
+            if (titleInput.value.length < 4) {
+                titleInput.setCustomValidity("Le titre doit contenir 4 lettres")
+            }
+        })
+
+        authorInput.addEventListener('input', (e) => {
+            authorInput.setCustomValidity("")
+            if (!authorInput.validity.valid) return
+            if (authorInput.value.length < 4) {
+                authorInput.setCustomValidity("Le nom de l'auteur doit contenir 4 lettres")
+            }
+        })
+
+
+
         this.#form.addEventListener('submit', (e) => {
             e.preventDefault()
             this.library.addBookToLibrary(e.target)
