3d7067112b90120de9736c19c5a1a9a060fe2ee4
jurol <jurol@example.com>
2026-01-03 18:49:08 +0100

    Update responsivv layout

--- classes/components/BookElement.js
+++ classes/components/BookElement.js
@@ -1,43 +1,45 @@
-import { Book } from "../models/Book.js"
+import { Book } from "../models/Book.js";
 
 export default class BookElement {
-    #element
-    constructor(/** @type {Book}*/book, library) {
-        this.#element = document.createElement('div')
-        this.#element.classList.add('card')
-        const title = document.createElement('h2')
-        const author = document.createElement('h3')
-        const pages = document.createElement('h4')
-        const readElement = document.createElement('p')
-        const btnElement = document.createElement('div')
-        if (book.read === true) {
-            readElement.textContent = "Lu"
-        } else {
-            readElement.textContent = "Pas lu"
-        }
-        const deleteBtn = document.createElement('button')
-        deleteBtn.classList.add('btn-delete')
-        deleteBtn.textContent = "Supprimer"
-        deleteBtn.dataset['id'] = book.id
-        deleteBtn.addEventListener('click', function (e) {
-            library.deleteBook(e.target.dataset['id'])
-        })
-        const readBtn = document.createElement('button')
-        readBtn.classList.add('btn')
-        readBtn.textContent = "Lu"
-        readBtn.addEventListener('click', function (/** @type {PointerEvent}*/e) {
-            library.updateBook(e.target.previousElementSibling.dataset['id'])
-        })
-        author.textContent = book.author
-        title.textContent = book.title
-        pages.textContent = book.pages + " pages"
-        this.#element.append(title)
-        this.#element.append(author)
-        this.#element.append(pages)
-        this.#element.append(readElement)
-        btnElement.append(deleteBtn)
-        btnElement.append(readBtn)
-        this.#element.append(btnElement)
+  #element;
+  constructor(/** @type {Book}*/ book, library) {
+    this.#element = document.createElement("div");
+    this.#element.classList.add("card");
+    const title = document.createElement("h2");
+    const author = document.createElement("h3");
+    const pages = document.createElement("h4");
+    const readElement = document.createElement("p");
+    const btnElement = document.createElement("div");
+    if (book.read === true) {
+      readElement.textContent = "Lu";
+    } else {
+      readElement.textContent = "Pas lu";
     }
-    get element() { return this.#element }
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("btn-delete");
+    deleteBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash-icon lucide-trash"><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/><path d="M3 6h18"/><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/></svg>`;
+    deleteBtn.dataset["id"] = book.id;
+    deleteBtn.addEventListener("click", function (e) {
+      library.deleteBook(e.target.dataset["id"]);
+    });
+    const readBtn = document.createElement("button");
+    readBtn.classList.add("btn");
+    readBtn.textContent = "Lu";
+    readBtn.addEventListener("click", function (/** @type {PointerEvent}*/ e) {
+      library.updateBook(e.target.previousElementSibling.dataset["id"]);
+    });
+    author.textContent = "Auteur: " + book.author;
+    title.textContent = "Titre: " + book.title;
+    pages.textContent = book.pages + " pages";
+    this.#element.append(title);
+    this.#element.append(author);
+    this.#element.append(pages);
+    this.#element.append(readElement);
+    btnElement.append(deleteBtn);
+    btnElement.append(readBtn);
+    this.#element.append(btnElement);
+  }
+  get element() {
+    return this.#element;
+  }
 }
